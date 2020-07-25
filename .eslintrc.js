19ed525b98b43a552ae45ee3b4ffaf3b90e64cc7
varlinor <varlinor@hotmail.com>
2020-07-26 00:11:13 +0800

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    root:true,
+    "env": {
+        "browser": false,
+        "commonjs": true,
+        "es2020": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 11
+    },
+    "rules": {
+    },
+    "extends": "airbnb"
+};
---
+++ bin/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+const fs=require('fs'),
+    path=require('path'),
+    {program}=require('commander');
+
